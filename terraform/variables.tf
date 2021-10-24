8438c3250e99cf1d76c76ce75aa6efbcaeaf00ae
beltranbot <beltranbot@gmail.com>
2021-10-24 12:01:10 -0500

    initial commit, load balancer asg and bastion

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "AWS_REGION" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+variable "BUCKET_NAME" {
+  type = string
+}
+
+variable "AMI_ID" {
+  type = string
+}
+
+variable "TAG_PREFIX" {
+  type = string
+}
+
+variable "PATH_TO_WEBSERVER_PUBLIC_KEY" {
+  type = string
+}
+
+variable "PATH_TO_BASTION_PUBLIC_KEY" {
+  type = string
+}
+
+variable "CD_DEPLOYMENT_GROUP" {
+  type = string
+}
+
+variable "VPC_PREFIX" {
+  type = string
+}
+
+variable "APP_PORT" {
+  type = number
+}
